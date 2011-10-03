7d5532b3147a2c60eaf274aef5a7d65ec2082e5b
Adminspot <jayfella@gmail.com>
2011-10-03 23:57:41 +0100

    Building Hangout Rows

--- javascripts/templates.js
+++ javascripts/templates.js
@@ -10,8 +10,13 @@ var templates = {
 	* Dynamic Hangout Row
 */
 
-templates.hangouts.row = '<article id="${htmlid}">';
+templates.hangouts.row = '<article id="${htmlid}" class="hangout" data-public="{{if public}}true{{else}}false{{/if}}">';
 	templates.hangouts.row += '<div class="top_nav">';
 		templates.hangouts.row += '<span>{{if public}}Pulic{{else}}Limited{{/if}}</span>';
+	templates.hangouts.row += '</div>';
+	templates.hangouts.row += '<div class="main_image rounded_image">';
+		templates.hangouts.row += '<img width="48" height="48" src="${clients[0].photo}?sz=48" />';
+	templates.hangouts.row += '</div>';
+	templates.hangouts.row += '<div class="info">';
 	templates.hangouts.row += '</div>';
 templates.hangouts.row += '</article>';
