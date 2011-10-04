059ac68ac00c0992c51c12511f7b9226352eb5fd
eshbeata <eshbeata@eshbeata-ubuntu.(none)>
2011-10-04 19:35:40 +0300

    UX , Design

--- javascripts/templates.js
+++ javascripts/templates.js
@@ -15,26 +15,31 @@ templates.hangouts.row = '<article id="${htmlid}" class="hangout" data-public="{
 		* Main image
 	*/
 	templates.hangouts.row += '<div class="main_image rounded_image">';
-		templates.hangouts.row += '<img alt="${clients[0].name}" title="${clients[0].name}" width="48" height="48" src="${clients[0].photo}?sz=48" />';
+		templates.hangouts.row += '<a href="#" ><img alt="${clients[0].name}" title="${clients[0].name}" width="48" height="48" src="${clients[0].photo}?sz=48" /></a>';
 	templates.hangouts.row += '</div>';
 
 	/*
 		* Right content area
 	*/
 	templates.hangouts.row += '<div class="info">';
-		templates.hangouts.row += '<div class="top_nav">';
-			templates.hangouts.row += '<span class="public"><a href="${post_url}">{{if public}}Pulic{{else}}Limited{{/if}}</a></span>';
-		templates.hangouts.row += '</div>';
+	
 		/*
 			* Client Images
 		*/
-		templates.hangouts.row += '<div class="cleints">';
-
+		templates.hangouts.row += '<div class="clients">';
+			templates.hangouts.row +='<p>Fuck You Robert</p>';
+			templates.hangouts.row +='<br class="clear" />';
+		
 			templates.hangouts.row += '{{each(i,v) clients}}';
-				templates.hangouts.row += '<img width="32" height="32" src="${v.photo}sz=32" />';
+				templates.hangouts.row +='{{if i!=0 }}';
+				templates.hangouts.row += '<a href="#"><img width="32" height="32" src="${v.photo}sz=32" /></a>';
+				templates.hangouts.row +='{{/if}}';
 			templates.hangouts.row += '{{/each}}';
 
 		templates.hangouts.row += '</div>';
-
+		templates.hangouts.row += '<div class="top_nav">';
+				templates.hangouts.row += '<span class="public"><a href="${post_url}">{{if public}}Pulic{{else}}Limited{{/if}}</a></span>';
+		templates.hangouts.row += '</div>';
+	
 	templates.hangouts.row += '</div>';
 templates.hangouts.row += '</article>';
