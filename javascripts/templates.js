aeecfb5491899ca55b2ead71e1e985481b546ed2
Adminspot <jayfella@gmail.com>
2011-10-04 16:43:21 +0100

    Minor Changes to Design

--- javascripts/templates.js
+++ javascripts/templates.js
@@ -11,13 +11,19 @@ var templates = {
 */
 
 templates.hangouts.row = '<article id="${htmlid}" class="hangout" data-public="{{if public}}true{{else}}false{{/if}}">';
-	templates.hangouts.row += '<div class="top_nav">';
-		templates.hangouts.row += '<span>{{if public}}Pulic{{else}}Limited{{/if}}</span>';
-	templates.hangouts.row += '</div>';
+	/*
+		* Main image
+	*/
 	templates.hangouts.row += '<div class="main_image rounded_image">';
-		templates.hangouts.row += '<img width="48" height="48" src="${clients[0].photo}?sz=48" />';
+		templates.hangouts.row += '<img alt="${clients[0].name}" title="${clients[0].name}" width="48" height="48" src="${clients[0].photo}?sz=48" />';
 	templates.hangouts.row += '</div>';
+
+	/*
+		* Right content area
+	*/
 	templates.hangouts.row += '<div class="info">';
-		templates.hangouts.row += '<p>Some text here that will be in each row</p>';
+		templates.hangouts.row += '<div class="top_nav">';
+			templates.hangouts.row += '<span class="public"><a href="${post_url}">{{if public}}Pulic{{else}}Limited{{/if}}</a></span>';
+		templates.hangouts.row += '</div>';
 	templates.hangouts.row += '</div>';
 templates.hangouts.row += '</article>';
